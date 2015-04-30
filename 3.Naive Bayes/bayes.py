0bdbe27a0acbd8782bc79fde0d91647760ccb78b
sunshine <sunshine46582747@sina.com>
2015-04-30 18:08:13 +0800

    Add spam classifier and RSS feed classifier.

--- 3.Naive Bayes/bayes.py
+++ 3.Naive Bayes/bayes.py
@@ -80,7 +80,8 @@ def testingNB():
 
 
 ######################################################################################
-def bagOfWords2Vec(vocabList, inputSet):
+#Spam classifier
+def bagOfWords2VecMN(vocabList, inputSet):
 	returnVec = [0]*len(vocabList)
 	for word in inputSet:
 		if word in vocabList:
@@ -95,6 +96,15 @@ def textParse(text):
 	tokens = [tok.lower() for tok in tokens if len(tok) > 2]
 
 	return tokens
+# def textParse(text):
+# 	import re
+	
+# 	tokens = re.compile('\\W*').split(text)
+# 	tokens = [tok.lower() for tok in tokens if len(tok) > 0]
+
+# 	return tokens
+
+
 
 def spamTest():
 	email_bodys = []
@@ -134,3 +144,105 @@ def spamTest():
 
 	print 'the error rate is: ',float(errorCount) / len(testing_data)
 	print 'the error count is: ',errorCount
+
+######################################################################################
+#RSS feed
+def calcMostFreq(words, text):
+	import operator
+	freqDict = {}
+	for word in words:
+		freqDict[word] = text.count(word)
+
+	sortedFreq = sorted(freqDict.iteritems(), key = operator.itemgetter(1), reverse = True)
+	return sortedFreq[:30]
+
+def localWords(feed1, feed0):
+	import feedparser
+	tokens_list = []
+	labels = []
+	full_text =[]
+	min_length = min(len(feed1['entries']), len(feed0['entries']))
+	for i in range(min_length):
+		tokens = advanced_reg_parse(feed1['entries'][i]['summary'])
+		tokens_list.append(tokens)
+		full_text.extend(tokens)
+		labels.append(1)
+
+		tokens = advanced_reg_parse(feed0['entries'][i]['summary'])
+		tokens_list.append(tokens)
+		full_text.extend(tokens)
+		labels.append(0)
+
+	#get and delte top-30 most frequent words
+	words = createVocabList(tokens_list)
+	top30Words = calcMostFreq(words, full_text)
+	for word in top30Words:
+		if word[0] in words:
+			words.remove(word[0])
+	
+	#randomly pick up 20 data, set them to testing data
+	#the remain data are used for training data
+	training_data_index = range(2 * min_length)
+	testing_data_index = []
+	for i in range(20):
+		randIndex = int(random.uniform(0,len(training_data_index)))
+		testing_data_index.append(training_data_index[randIndex])
+		del(training_data_index[randIndex])
+
+	#train
+	training_data_mat = []
+	training_data_labels = []
+	for docIndex in training_data_index:
+		training_data_mat.append(bagOfWords2VecMN(words, tokens_list[docIndex]))
+		training_data_labels.append(labels[docIndex])
+	p1V,p0V,pSpam = trainNB0(array(training_data_mat),array(training_data_labels))
+
+	#test
+	errorCount = 0
+	for docIndex in testing_data_index:
+		wordVector = bagOfWords2VecMN(words, tokens_list[docIndex])
+		if classifyNB(array(wordVector),p0V,p1V,pSpam) != labels[docIndex]:
+			errorCount += 1
+	print 'the error rate is: ',float(errorCount)/len(testing_data_index)
+	return words,p0V,p1V
+
+def getTopWords(ny,sf):
+	import operator
+
+	words, p0, p1 = localWords(ny,sf)
+	top1 = []
+	top0 = []
+	for i in range(len(p0)):
+		if p0[i] > -6.0 :
+			top0.append((words[i], p0[i]))
+		if p1[i] > -6.0 :
+			top1.append((words[i], p1[i]))
+	
+	sortedSF = sorted(top0, key = lambda pair: pair[1], reverse = True)
+	print "\nSF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF**SF\n"
+	
+	cnt = 0
+	for item in sortedSF:
+		print item[0]
+		if cnt == 10:
+			break
+		else :
+			cnt += 1
+	
+	sortedNY = sorted(top1, key=lambda pair: pair[1], reverse=True)
+	print "\nNY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY**NY\n"
+	
+	cnt = 0
+	for item in sortedNY:
+		print item[0]
+		if cnt == 10:
+			break
+		else :
+			cnt += 1
+
+def advanced_reg_parse(text):
+	import re
+	tokens = re.compile('\\W*').split(text)
+	tokens = [tok.lower() for tok in tokens if len(tok) > 2]
+
+	return tokens
